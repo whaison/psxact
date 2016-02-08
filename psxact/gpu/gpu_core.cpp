db74611c581c599980e656f3d50baddeb4f19ed9
beannaich <beannaich@gmail.com>
2016-02-07 22:48:48 -0700

    Fleshing out the GPU memory interface a bit.

--- psxact/gpu/gpu_core.cpp
+++ psxact/gpu/gpu_core.cpp
@@ -2,8 +2,40 @@
 
 using namespace gpu;
 
+#define REG_BEGIN 0x1f801810
+
 core_t::core_t() {
 }
 
 core_t::~core_t() {
+}
+
+uint32_t core_t::read(uint32_t address) {
+    switch (address - REG_BEGIN) {
+    case 0: return read_resp();
+    case 4: return read_stat();
+    }
+
+    return 0;
+}
+
+void core_t::write(uint32_t address, uint32_t data) {
+    switch (address - REG_BEGIN) {
+    case 0: return write_gp0(data);
+    case 4: return write_gp1(data);
+    }
+}
+
+uint32_t core_t::read_resp() {
+    return 0;
+}
+
+uint32_t core_t::read_stat() {
+    return 0;
+}
+
+void core_t::write_gp0(uint32_t command) {
+}
+
+void core_t::write_gp1(uint32_t command) {
 }
--- psxact/gpu/gpu_core.hpp
+++ psxact/gpu/gpu_core.hpp
@@ -1,11 +1,27 @@
 #ifndef __gpu_core_hpp__
 #define __gpu_core_hpp__
 
+#include <stdint.h>
+
 namespace gpu {
     class core_t {
     public:
         core_t();
+
         ~core_t();
+
+        uint32_t read(uint32_t);
+
+        void write(uint32_t, uint32_t);
+
+    private:
+        uint32_t read_resp();
+
+        uint32_t read_stat();
+
+        void write_gp0(uint32_t);
+
+        void write_gp1(uint32_t);
     };
 }
 
